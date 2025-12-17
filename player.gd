381a5fae2501a29e2e19c604cf0f86bdd364c5d0
Jake Klingler <thearrow3456@gmail.com>
2025-12-16 22:20:14 -0500

    player death

--- player.gd
+++ player.gd
@@ -10,6 +10,9 @@ extends CharacterBody3D
 # meters per second.
 @export var bounce_impulse = 16
 
+# Emitted when the player was hit by a mob.
+signal hit
+
 var target_velocity = Vector3.ZERO
 
 func _physics_process(delta):
@@ -72,3 +75,10 @@ func _physics_process(delta):
 	# Moving the Character
 	velocity = target_velocity
 	move_and_slide()
+
+func die():
+	hit.emit()
+	queue_free()
+
+func _on_mob_detector_body_entered(body: Node3D) -> void:
+	die()
